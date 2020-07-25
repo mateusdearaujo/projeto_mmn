6a1bacfa5b38863e4dcdf5d636d233dd518fc830
Mateus Araujo <mateusdearaujo@hotmail.com.br>
2020-07-25 17:50:44 -0300

    Transformando  em global e adicionando limite de filhos

--- config.php
+++ config.php
@@ -1,6 +1,9 @@
 <?php
 try {
+    global $pdo;
     $pdo = new PDO("mysql:dbname=projeto_mmn;host=localhost:3306", "mateus", "");
 } catch(PDOException $e) {
     echo "Erro: ".$e->getMessage();
 }
+
+$limite = 3;
